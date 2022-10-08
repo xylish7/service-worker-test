e2dc685b19075e0cc42932db9a23182ac8dcca2e
Filip Frincu <filip.frincu@visma.com>
2022-10-08 16:22:25 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 function App() {
+  useEffect(() => {
+    serviceWorkerRegistration.register();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
